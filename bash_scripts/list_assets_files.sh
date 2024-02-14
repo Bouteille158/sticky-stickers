c04bc92b700595f63d847cf1542978d743fbe862
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-02-14 13:15:34 +0100

    feat: make scripts generate files

--- bash_scripts/list_assets_files.sh
+++ bash_scripts/list_assets_files.sh
@@ -3,16 +3,29 @@
 # Script to list all files in the assets folder and its subfolders
 # Save the list to a text file
 
+# Usage:
+# 1. Open Terminal
+# 2. Run the script:
+#    bash /path/to/list_assets_files.sh
+# 3. The script will generate a text file with the list of files in the assets folder and its subfolders
+#    The file will be named 'cached-files-list.txt' and saved to the root folder
+
 # Specify the target folder
-target_folder="/path/to/assets/folder"
+target_folder="./assets/"
 
 # Specify the output text file
-output_file="assets_file_list.txt"
+output_file="../cached-files-list.txt"
+
+# Rename the previous cached-files-list.txt file to old.cached-files-list-timestamp.txt
+if [ -f "$output_file" ]; then
+    echo "Renaming previous file list to old.cached-files-list-$(date +%s).txt"
+    mv "$output_file" "old.cached-files-list-$(date +%s).txt"
+fi
 
 # Change to the target folder
 cd "$target_folder" || exit
 
 # List all files in the folder and its subfolders, save to the text file
-find . -type f -not -name "${output_file}" -not -name ".DS_Store" | sed 's|^\./|/assets/|' > "$output_file"
+find . -type f -not -name "${output_file}" -not -name ".DS_Store" | sed 's|^\./|/assets/|' | sort > "$output_file"
 
 echo "File list generated and saved to: $output_file"
--- bash_scripts/list_filenames.sh
+++ bash_scripts/list_filenames.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
-# Script to list all files in the a folder and its subfolders
+# Script to list all assets files in the a folder and its subfolders
 # Save the list to a text file
 
+# Usage:
+# 1. Open Terminal
+# 2. Run the script:
+#    bash /path/to/list_filenames.sh
+#
+# Note: Replace '/path/to/list_filenames.sh' with the actual path to the script.
+
 # Specify the target folder
-target_folder="/path/to/target/folder"
+target_folder="assets/*"
 
-# Specify the output text file
-output_file="_file_list.txt"
+# Rename the previous list-list.txt file to old.list-list-timestamp.txt
+if [ -f "lists-list.txt" ]; then
+    echo "Renaming previous file list to old.lists-list-$(date +%s).txt"
+    mv "lists-list.txt" "old.lists-list-$(date +%s).txt"
+fi
 
-# Change to the target folder
-cd "$target_folder" || exit
+# For each folder in the assets folder, run the script
+for folder in $target_folder
+do
+    echo "Processing folder: $folder"
 
-# List only filenames and extensions, excluding the output file, save to the text file
-find . -type f -not -name "${output_file}" -not -name ".DS_Store" -exec basename {} \; | sort > "$output_file"
+    # Specify the output text file
+    output_file="_file_list.txt"
 
+    # Change to the target folder
+    cd "$folder" || exit
 
-echo "File list generated and saved to: $output_file"
+    # List only filenames and extensions, excluding the output file, save to the text file
+    find . -type f -not -name "${output_file}" -not -name ".DS_Store" -exec basename {} \; | sort > "$output_file"
+  
+    echo "./$folder/$output_file" >> ../../lists-list.txt
+
+    echo "File list generated and saved to: $output_file"
+
+    # Go back to the root folder
+    cd ../..
+done
