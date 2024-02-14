c23534f57d5c92eb5a4ee1682f211cb7a97b5e6a
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-02-14 13:29:30 +0100

    feat: create setup script to generate needed files

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+## Moving the current working directory for local sources
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
+cd "$parent_path" || exit
+
+# Create a new folder for the assets
+mkdir -p assets
+
+# Launch the script to generate lists-list.txt file 
+# with the list of files in the assets folder and its subfolders
+bash ./bash_scripts/list_filenames.sh
+
+# Launch the script to generate cached-files-list.txt file
+# with the list of files in the assets folder and its subfolders
+bash ./bash_scripts/list_assets_files.sh
